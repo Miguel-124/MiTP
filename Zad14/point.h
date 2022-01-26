c19091c5178c7d99f11736bd738a33c14fd038f3
Miguel-124 <96996981+Miguel-124@users.noreply.github.com>
2022-01-26 14:03:58 +0100

    Create point.h

---
+++ Zad14/point.h
@@ -0,0 +1,10 @@
+struct POINT;
+
+float area(struct POINT a, struct POINT b, struct POINT c);
+float perimeter(struct POINT a, struct POINT b, struct POINT c);
+
+struct POINT
+{
+    int x;
+    int y;
+};
